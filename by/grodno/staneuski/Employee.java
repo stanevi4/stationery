7dcf6bc6d8f39ba5c5bd301bc8ff549df95d5d4d
siarhei <sstbox@gmail.com>
2016-04-03 11:42:23 +0300

    commit

--- by/grodno/staneuski/Employee.java
+++ by/grodno/staneuski/Employee.java
@@ -1,10 +1,12 @@
 package by.grodno.staneuski;
 
+import java.util.ArrayList;
+
 public class Employee {
 	
 	private String name;
 	private String position;
-	private Workspace workspace;
+	private ArrayList<StationeryAccessories> workspace;
 	
 	public String getName() {
 		return name;
@@ -17,32 +19,5 @@ public class Employee {
 	}
 	public void setPosition(String position) {
 		this.position = position;
-	}
-	public Workspace getWorkspace() {
-		return workspace;
-	}
-	public void setWorkspace(Workspace workspace) {
-		this.workspace = workspace;
-	}
-	
-	public void addItem(StationeryAccessories item){
-		this.workspace.addToWorkspace(item);
-	}
-	public void removeItem(StationeryAccessories item){
-		this.workspace.addToWorkspace(item);
-	}
-	
-	public void printList(){
-		int i = 1;
-		System.out.println(this.name + " workspace content:");
-		for(StationeryAccessories item: workspace.getStorage()){
-			System.out.print(i + ". ");
-			System.out.println(item.toString());
-			i++;
-		}
-	}
-	public void printSumm(){
-		int summ = workspace.getSumm();
-		System.out.println("Summ = " + summ);
 	}
 }
